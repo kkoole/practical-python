f46bf5be6273f398e695ea902bea772b23bd389b
Kaj Koole <kajkoole@outlook.com>
2022-03-07 21:54:12 +0100

    Exercises 1.2

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,12 @@
 # bounce.py
 #
 # Exercise 1.5
+
+height = 100 # 100 meters
+bounces = 10 # amount of bounces
+bounce = 0 # current bounce
+
+while bounce < bounces:
+    height = height * 0.6
+    print(round(height, 4))
+    bounce = bounce + 1
