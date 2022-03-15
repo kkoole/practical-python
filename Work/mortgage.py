c8b068c32b1488e3e3cfd60150b2e61adadfce25
Kaj Koole <kajkoole@outlook.com>
2022-03-15 19:35:36 +0100

    Excercise 1.11

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -8,13 +8,23 @@ payment = 2684.11
 total_paid = 0.0
 months_paid = 0
 
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+extra_payment = 1000
+
 while principal > 0:
-    if (months_paid < 12):
-        principal = principal * (1+rate/12) - (payment + 1000)
-        total_paid = total_paid + (payment + 1000)
+    if principal < payment:
+        payment = principal
+        principal = principal - payment
+        total_paid = total_paid + payment
+    elif (months_paid >= extra_payment_start_month and months_paid <= extra_payment_end_month):
+        principal = principal * (1+rate/12) - (payment + extra_payment)
+        total_paid = total_paid + (payment + extra_payment)
     else: 
         principal = principal * (1+rate/12) - payment
         total_paid = total_paid + payment
     months_paid = months_paid + 1
+    
+    print(months_paid, round(total_paid, 2), round(principal, 2))
 
-print('Total paid', total_paid, 'over', months_paid, 'months')
+print('Total paid', round(total_paid, 2), '\nMonths', months_paid)
