e831e0d9c092d8419227ffa6b11c26273fe81cca
Kaj Koole <kajkoole@outlook.com>
2022-03-16 11:38:10 +0100

    Exercise 1.31

--- Work/pcost.py
+++ Work/pcost.py
@@ -12,7 +12,10 @@ def portfolio_cost(filename):
     for line in f:
         row = line.split(',')
 
-        total_cost = total_cost + (int(row[1]) * float(row[2]))
+        try:
+            total_cost = total_cost + (int(row[1]) * float(row[2]))
+        except ValueError:
+            print('That\'s an oopsie')
 
     f.close()
 
