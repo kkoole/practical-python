1495dfb6a3694e475e1b2a1adf1e5892568adab6
Kaj Koole <kajkoole@outlook.com>
2022-03-16 14:56:46 +0100

    Exercise 2.11

--- Work/report.py
+++ Work/report.py
@@ -49,6 +49,10 @@ portfolio = read_portfolio('Data/portfolio.csv')
 prices = read_prices('Data/prices.csv')
 
 report = make_report(portfolio, prices)
+headers = ('Name', 'Shares', 'Price', 'Change')
+
+print('%10s %10s %10s %10s' % headers)
+print('---------- ' * len(headers))
 
 for name, shares, price, change in report:
     print(f'{name:>10s} {shares:>10d} {price:>10.2f} {change:>10.2f}')
